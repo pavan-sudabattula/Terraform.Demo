70e5415cfe53c797e271acb3ec8fb0cad695f089
Pavan Sudabattula <pavan.sudabattula@tal.com.au>
2021-07-09 13:51:15 +1000

    created module for azure CDN creation for static website

---
+++ variables.tf
@@ -0,0 +1,25 @@
+variable "storage_account_name" {
+  default = "storageaccmelb01"
+}
+
+variable "static_website_source_folder" {
+  default = "../artifacts/imagine"
+}
+
+variable "cdn_profile_name" {
+  default = "mycdnprofile-melb-01"
+}
+
+variable "cdn_sku_profile" {
+  default = "Standard_Microsoft"
+}
+
+variable "index_path" {
+  description = "path from your repo root to index.html"
+  default     = "index.html"
+}
+
+variable "custom_404_path" {
+  description = "path from your repo root to your custom 404 page"
+  default     = "404.html"
+}
---
+++ versions.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 2.59.0"
+    }
+    random = {
+      source = "hashicorp/random"
+    }
+    null = {
+      source = "hashicorp/null"
+    }
+  }
+  required_version = ">= 0.13"
+}
+
+provider "azurerm" {
+  features {}
+}
